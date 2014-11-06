18405bf1857f64ec541f0b596ef9e41f25ec29a1
David Reiss <dreiss@systemsbiology.org>
2014-11-05 18:23:36 -0800

    lotsa changes.

--- egrin2/fimo.py
+++ egrin2/fimo.py
@@ -1,7 +1,8 @@
 #!/tools/bin/python
-""" egrin2-fimo.py
-Author:  Micheleen M. Harris
-Description:  FIMO step of the EGRIN2 pipeline.  This program makes a script which can be submitted to an SGE scheduler on a cluster.
+""" fimo.py
+Author:  Micheleen M. Harris, David J Reiss
+Description:  FIMO step of the EGRIN2 pipeline.  This program makes a script which can be submitted to 
+an SGE scheduler on a cluster.
 
 This script assumes an ensemble run under a directory structure that 
 is as follows:
@@ -19,9 +20,11 @@ Note:  if you are using c-shell make sure to use the --csh flag when running thi
 
 Example:
 
-python egrin2-fimo.py --seqs_file cache/Escherichia_coli_K12_NC_000913.2 --user mharris --base_dir . --target_dir . --qsub_script fimo_batch_script.sh --num_cores 1  --csh --organism_name eco
+python fimo.py --seqs_file cache/Escherichia_coli_K12_NC_000913.2 --user mharris --base_dir . 
+                --target_dir . --qsub_script fimo_batch_script.sh --num_cores 1  --csh --organism_name eco
 
-Output will be in subdirectory to input directory (<prefix>001 for example will have all fimo files associated with the meme files).
+Output will be in subdirectory to input directory (<prefix>001/fimo_outs/ for example will have all fimo 
+files associated with the meme files).
 """
 
 import optparse
@@ -56,7 +59,7 @@ QSUB_TEMPLATE = """#$ -S /bin/bash
 #$ -t 1-%s
 #$ -cwd
 #$ -pe serial %s
-#$ -l mem_free=32G
+#$ -l mem_free=8G
 
 %s"""
 
@@ -80,173 +83,171 @@ QSUB_TEMPLATE_CSH = """#$ -S /bin/csh
 #$ -t 1-%s
 #$ -cwd
 #$ -pe serial %s
-#$ -l mem_free=32G
+#$ -l mem_free=8G
 
 %s"""
 
 def fix_meme_files(meme_files):
-
-	newmemes = [] # The new file names - TODO:  can get rid of this by just using os to move the name to original
-
-	#  Read memefile line by line and output line by line, replacing strange e-value formats (e.g. "E= 10.0e+003" to "E= 1.0e+004")
-	for memef in meme_files:
-		IN = open(memef,'rb')
-		OUT = open(memef+'4fimo', 'wb')
-		for line in IN:
-			if 'letter-probability matrix' in line: # Note: fimo uses this matrix for input
-				linespl = line.split()
-				evalue = linespl[9].split('e')
-				p1 = float(evalue[0]) # first part of evalue, want format to be 1.0 instead of 10.0 for e.g.
-				if p1 >= 10.0: # this is something we need to change!
-					p1 /= 10. # shift decimal
-					p1 = round(p1,1) # round to 1 decimal place
-					p2 = evalue[1] # second part of evalue, will have a + or - at beginning
-					newevalue = ''
-					if '+' in p2:
-						p2 = p2.replace('+','')
-						if p2 == '000':
-							p2 = 0
-							newevalue = "%se+%03d" % (str(p1),p2)
-						else:
-							p2 = int(p2.lstrip("0")) + 1 # now an int
-							newevalue = "%se+%03d" % (str(p1),p2)
-					else:
-						p2 = p2.replace('-','')
-						if p2 == '000':
-							p2 = 0
-							newevalue = "%se+%03d" % (str(p1),p2)
-						else:
-							p2 = int(p2.lstrip("0")) - 1 # now an int
-							newevalue = "%se-%03d" % (str(p1),p2)
-					linespl[9] = newevalue
-					OUT.write(' '.join(linespl)+'\n')
-			else:
-				OUT.write(line)
-		IN.close()
-		OUT.close()
-		os.rename((memef+'4fimo'),memef)
+    #  Read memefile line by line and output, replacing strange e-value formats (e.g. "E= 10.0e+003" to "E= 1.0e+004")
+    for memef in meme_files:
+        IN = open(memef,'rb')
+        OUT = open(memef+'_fimo', 'wb')
+        for line in IN:
+            if 'letter-probability matrix' in line: # Note: fimo uses this matrix for input
+                linespl = line.split()
+                evalue = linespl[9].split('e')
+                p1 = float(evalue[0]) # first part of evalue, want format to be 1.0 instead of 10.0 for e.g.
+                if p1 >= 10.0: # this is something we need to change!
+                    p1 /= 10. # shift decimal
+                    p1 = round(p1,1) # round to 1 decimal place
+                    p2 = evalue[1] # second part of evalue, will have a + or - at beginning
+                    newevalue = ''
+                    if '+' in p2:
+                        p2 = p2.replace('+','')
+                        if p2 == '000':
+                            p2 = 0
+                            newevalue = "%se+%03d" % (str(p1),p2)
+                        else:
+                            p2 = int(p2.lstrip("0")) + 1 # now an int
+                            newevalue = "%se+%03d" % (str(p1),p2)
+                    else:
+                        p2 = p2.replace('-','')
+                        if p2 == '000':
+                            p2 = 0
+                            newevalue = "%se+%03d" % (str(p1),p2)
+                        else:
+                            p2 = int(p2.lstrip("0")) - 1 # now an int
+                            newevalue = "%se-%03d" % (str(p1),p2)
+                    linespl[9] = newevalue
+                    OUT.write(' '.join(linespl)+'\n')
+            else:
+                OUT.write(line)
+        IN.close()
+        OUT.close()
+        os.rename((memef+'_fimo'),memef)
 
 
 def main():
+    #  Collect & check args
+    op = optparse.OptionParser()
+    op.add_option('-g', '--seqs_file', help='The sequence file (genome) (found in cache/<organism name>')
+    op.add_option('-o', '--organism_name', help='KEGG name for organism (e.g. eco, hal')
+    op.add_option('-u', '--user', help='User name on cluster')
+    op.add_option('-i', '--base_dir', default='.', help='Cmonkey-python base directory.')
+    op.add_option('-t', '--target_dir', default='.', help='The output directory name')
+    op.add_option('-q', '--qsub_script', default='fimo_batch_script.sh', help='The script name for running fimo on cmonkey results')
+    op.add_option('-n', '--num_cores', default=2, help='Number of cores to use on cluster')
+    op.add_option('-s', '--csh', help='If c-shell indicate with this flag', action='store_true')
+    op.add_option('-f', '--fix', default=False, help='Fix meme files', action='store_true')
+    opt, args = op.parse_args()
 
-	#  Collect & check args
+    if not opt.seqs_file:
+        op.error('need --seqs_file option.  Use -h for help.')
+    if not opt.target_dir:
+        op.error('need --target_dir option.  Use -h for help.')
+    if not opt.base_dir:
+        op.error('need --base_dir option.  Use -h for help.')
+    if not opt.num_cores:
+        op.error('need --num_cores option.  Use -h for help.')
+    if not opt.qsub_script:
+        op.error('need --qsub_script option.  Use -h for help.')
+    if not opt.organism_name:
+        op.error('need --organism_name option.  Use -h for help.')
+    if not opt.user:
+        op.error('need --user option.  Use -h for help.')
+    if not opt.num_cores:
+        op.error('need --num_cores option.  Use -h for help.')
 
-	op = optparse.OptionParser()
-	op.add_option('-g', '--seqs_file', help='The sequence file (genome) (found in cache/<organism name>')
-	op.add_option('-o', '--organism_name', help='KEGG name for organism (e.g. eco, hal')
-	op.add_option('-u', '--user', help='User name on cluster')
-	op.add_option('-i', '--base_dir', help='Cmonkey-python base directory.')
-	op.add_option('-t', '--target_dir', help='The output directory name')
-	op.add_option('-q', '--qsub_script', help='The script name for running fimo on cmonkey results')
-	op.add_option('-n', '--num_cores', help='Number of cores to use on cluster')
-	op.add_option('-s', '--csh', help='If c-shell indicate with this flag', action='store_true')
-	opt, args = op.parse_args()
+    if opt.csh:
+        header = QSUB_TEMPLATE_HEADER_CSH
+        template = QSUB_TEMPLATE_CSH
+        shellheader = SHELL_HEADER_CSH
+    else:
+        header = QSUB_TEMPLATE_HEADER
+        template = QSUB_TEMPLATE
+        shellheader = SHELL_HEADER
 
-	if not opt.seqs_file:
-		op.error('need --seqs_file option.  Use -h for help.')
-	if not opt.target_dir:
-		op.error('need --target_dir option.  Use -h for help.')
-	if not opt.base_dir:
-		op.error('need --base_dir option.  Use -h for help.')
-	if not opt.num_cores:
-		op.error('need --num_cores option.  Use -h for help.')
-	if not opt.qsub_script:
-		op.error('need --qsub_script option.  Use -h for help.')
-	if not opt.organism_name:
-		op.error('need --organism_name option.  Use -h for help.')
-	if not opt.user:
-		op.error('need --user option.  Use -h for help.')
-	if not opt.num_cores:
-		op.error('need --num_cores option.  Use -h for help.')
+    # Fix genome seqs file for fimo (to upper and add header), ouput to new file, put in input dir (might be better way)
+    seqsfile_in = open(opt.seqs_file, 'rb')
+    lines = seqsfile_in.readlines()
+    genomeseq = lines[0].upper() # to upper may not be necessary
 
-	if opt.csh:
-		header = QSUB_TEMPLATE_HEADER_CSH
-		template = QSUB_TEMPLATE_CSH
-		shellheader = SHELL_HEADER_CSH
-	else:
-		header = QSUB_TEMPLATE_HEADER
-		template = QSUB_TEMPLATE
-		shellheader = SHELL_HEADER
+    seqsfile_out = os.path.join(opt.seqs_file)+'_fimo'
+    out = open(seqsfile_out, 'wb')
+    out.write(">"+os.path.basename(opt.seqs_file)+"\n") # The short little header needed by fimo
+    out.write(genomeseq)
+    out.close()
 
+    #  Create a dict of run output dirs with array of meme file names
+    org_out_dirs = glob.glob(os.path.join(opt.base_dir,"%s-out-*" % opt.organism_name))
+    ##out_dir_dict = {}
 
-	#  Fix genome seqs file for fimo (to upper and add header), ouput to new file, put in input dir (might be better way...)
-	seqsfile_in = open(opt.seqs_file, 'rb')
-	lines = seqsfile_in.readlines()
-	genomeseq = lines[0].upper() # to upper may not be necessary
+    for org_dir in org_out_dirs:
+        print org_dir
+        #  Find MEME files
+        meme_files = glob.glob(os.path.join(org_dir, "meme-out-*"))
+        if opt.fix:
+            fix_meme_files(meme_files) # Fixes e-value formats  - TODO: make more efficient
+        ##out_dir_dict[org_dir] = meme_files
 
-	seqsfile_out = os.path.join(opt.seqs_file)+'4fimo'
-	out = open(seqsfile_out, 'wb')
-	out.write(">"+os.path.basename(opt.seqs_file)+"\n") # The short little header needed by fimo
-	out.write(genomeseq)
-	out.close()
+        #  We will make a subscripts for each run directory to call fimo on all meme files within that directory
+        ##sub_scripts = {}
+        ##for org_dir in org_out_dirs:
+        ##meme_files = out_dir_dict[org_dir]
 
-	#  Create a dict of run output dirs with array of meme file names
-	org_out_dirs = glob.glob(os.path.join(opt.base_dir,"%s-out-*" % opt.organism_name))
+        try:
+            os.mkdir(os.path.join(org_dir, "fimo-outs")) ## location of output files
+        except:
+            None
 
-	out_dir_dict = {}
+        #  We will make a subscript for each run directory to call fimo on all meme files within that directory
+        fimo_cmd = ""
+        for meme in meme_files:
+            num = os.path.basename(meme).split('-')[3] # Get the bicluster number
+            #num = num[1:] # remove leading 0
+            fimo_cmd += '\n' + 'echo "%s"' % (meme) + '\n'
+            fimo_cmd += '\n' + FIMO_TEMPLATE % (meme, seqsfile_out, os.path.join(org_dir, "fimo-outs/fimo-out-%s" % num)) + '\n'
 
-	for org_dir in org_out_dirs:
-		#  Find MEME files
-		meme_files = glob.glob(os.path.join(org_dir, "meme-out-*"))
-		fix_meme_files(meme_files) # Fixes e-value formats (iterates thru every line of MEME file - TODO: make more efficient)
-		out_dir_dict[org_dir] = meme_files
+        ##sub_scripts[org_dir] = fimo_cmd
 
-	#  We will make a subscripts for each run directory to call fimo on all meme files within that directory
-	sub_scripts = {}
-	for org_dir in org_out_dirs:
-		meme_files = out_dir_dict[org_dir]
-		fimo_cmd = ""
+    ##for org_dir in org_out_dirs:
+        outfile_name = os.path.join(org_dir, 'fimo_script.sh')
+        with open(outfile_name, 'w') as outfile:
+            subscript_template = shellheader + fimo_cmd ##sub_scripts[org_dir]
+            outfile.write(shellheader)
+            outfile.write(fimo_cmd) ##sub_scripts[org_dir])
+            #rendered = Template(subscript_template).render()
+            #outfile.write(rendered)
+            #st = os.stat(outfile)
+            #os.chmod(subscript, st.st_mode | stat.S_IEXEC)
+        outfile.close()
+        #st = os.stat(outfile)
+        #os.chmod(subscript, st.st_mode | stat.S_IEXEC)
+        os.chmod(outfile_name,0744)
 
-		#  We will make a subscript for each run directory to call fimo on all meme files within that directory
-		for meme in meme_files:
-			num = os.path.basename(meme).split('-')[3] # Get the run number
-			num = num[1:] # remove leading 0
-			fimo_cmd += '\n' + FIMO_TEMPLATE % (meme, seqsfile_out, os.path.join(org_dir, "fimo-out-%s" % num)) + '\n'
+    #  Create master script
+    with open(os.path.join(opt.base_dir, opt.qsub_script), 'w') as outfile:
+        if opt.user is not None:
+            login = opt.user
+        else:
+            os.getlogin()
 
-		sub_scripts[org_dir] = fimo_cmd
+        num_runs = len(org_out_dirs)
+        subscript = "%s-out-${BATCHNUM}/fimo_script.sh >& %s-out-${BATCHNUM}/fimo_script.sh.out" % (opt.organism_name, \
+                                                                                                    opt.organism_name)
 
-	for org_dir in org_out_dirs:
-		outfile_name = os.path.join(org_dir, 'fimo_script.sh')
-		with open(outfile_name, 'w') as outfile:
-			subscript_template = shellheader + sub_scripts[org_dir]
-			outfile.write(shellheader)
-			outfile.write(sub_scripts[org_dir])
-			#rendered = Template(subscript_template).render()
-			#outfile.write(rendered)
-			#st = os.stat(outfile)
-			#os.chmod(subscript, st.st_mode | stat.S_IEXEC)
-		outfile.close()
-		#st = os.stat(outfile)
-		#os.chmod(subscript, st.st_mode | stat.S_IEXEC)
-		os.chmod(outfile_name,0744)
+        outfile.write(header)
+        outfile.write(template % (login, 
+            login,
+            num_runs,
+            opt.num_cores, 
+            subscript))
 
-
-
-	#  Create master script
-	with open(os.path.join(opt.base_dir, opt.qsub_script), 'w') as outfile:
-		if opt.user is not None:
-			login = opt.user
-		else:
-			os.getlogin()
-
-		num_runs = len(org_out_dirs)
-
-		subscript = "%s-out-${BATCHNUM}/fimo_script.sh" % (opt.organism_name)
-
-
-		outfile.write(header)
-		outfile.write(template % (login, 
-			login,
-			num_runs,
-			opt.num_cores, 
-			subscript))
-
-	outfile.close()
-
+    outfile.close()
 
 if __name__ == '__main__':
-	main()
-	
+    main()
+    
 
 
 
--- egrin2/run_coding_fracs.py
+++ egrin2/run_coding_fracs.py
@@ -1,144 +1,58 @@
-#!/tools/bin/python
-
 import optparse
-import sys
-import subprocess
 import os
 import glob
-import csv
 import bz2
 
-import time
+import pandas as pd
+import numpy as np
 
 def main():
+    #  Collect & check args
+    op = optparse.OptionParser()
+    op.add_option('-c', '--cache_dir', help='The cmpython cache directory where the genome features and info live')
+    op.add_option('-f', '--features_file', help='The features file (with coding regions) (found in cache/<organism name>features')
+    op.add_option('-o', '--organism_name', help='The organism name (e.g. eco or hal)')
+    op.add_option('-i', '--input_dir', help='The cmonkey run input dir where the fimo files live (e.g. eco-out-001)')
+    opt, args = op.parse_args()
 
-	#  Collect & check args
+    if not opt.cache_dir:
+        op.error('need --cache_dir option.  Use -h for help.')
+    if not opt.features_file:
+        op.error('need --features_file option.  Use -h for help.')
+    if not opt.organism_name:
+        op.error('need --organism_name option.  Use -h for help.')
+    if not opt.input_dir:
+        op.error('need --input_dir option.  Use -h for help.')
 
-	op = optparse.OptionParser()
-	op.add_option('-c', '--cache_dir', help='The cmpython cache directory where the genome features and info live')
-	op.add_option('-f', '--features_file', help='The sequence file (with coding regions) (found in cache/<organism name>features')
-	op.add_option('-o', '--organism_name', help='The organism name (e.g. eco or hal)')
-	op.add_option('-i', '--input_dir', help='The cmonkey run input dir where the fimo files live (e.g. eco-out-001)')
-	opt, args = op.parse_args()
+    fimo_files = np.sort( np.array( glob.glob(os.path.join(opt.input_dir, "fimo-outs/fimo-out-*")) ) )
+    print 'Number of fimo files = ', str(len(fimo_files))
 
-	if not opt.cache_dir:
-		op.error('need --cache_dir option.  Use -h for help.')
-	if not opt.features_file:
-		op.error('need --features_file option.  Use -h for help.')
-	if not opt.organism_name:
-		op.error('need --organism_name option.  Use -h for help.')
-	if not opt.input_dir:
-		op.error('need --input_dir option.  Use -h for help.')
+    #  Get coding regions from features file (e.g. Escherichia_coli_K12_features)
+    f = open(opt.features_file, 'r')
+    skip = 1
+    line = f.readline()
+    while 'header' not in line:
+        line = f.readline()
+        skip += 1
+    f.close()
 
-	fimo_files = glob.glob(opt.input_dir + "/fimo-outs/fimo-out-*")
-	print 'Number of fimo files = ', str(len(fimo_files))
+    features = pd.read_table(opt.features_file,engine='python',skiprows=skip)
+    features = features[ features.type != 'SEQ_END' ]
 
-	fimodict = {}
+    for f in fimo_files:
+        print f
+        fimo = pd.read_table( bz2.BZ2File(f) )
+        is_bad = np.zeros( fimo.shape[0], dtype=bool )
+        for i in xrange(fimo.shape[0]):
+            row = fimo.ix[i]
+            hits = features[ (features.start_pos <= row.start) & (features.end_pos >= row.start) ]
+            if hits.shape[0] <= 0:
+                hits = features[ (features.start_pos <= row.stop) & (features.end_pos >= row.stop) ]
+            if hits.shape[0] > 0:
+                is_bad[i] = True
 
-	for i in range(0,len(fimo_files)):
-		fin = fimo_files[i]
-		bc = os.path.basename(fin)
-
-#		with open(fin, 'rb') as f:
-		with bz2.BZ2File(fin, 'rb') as f:
-			
-			innerdict = {}
-			cnt = 0
-
-			for line in csv.DictReader(f, dialect='excel-tab'):
-				
-				strand = line['strand']
-				motif = line['matched sequence']
-
-				if strand is '+':
-					stop = line['stop']
-				if strand is '-':
-					stop = line['start']
-
-				innerdict[motif] = (stop, strand)
-
-			fimodict[bc] = innerdict
-
-		f.close()
-
-
-	#  Get coding regions from features file (e.g. Escherichia_coli_K12_features)
-	#  Simple CDS array of tuples containing starts and strands 
-	cdsstarts = []
-	cdsstops = []
-	cdsstrands = []
-
-	with open(opt.features_file, 'rb') as f:
-
-		line = f.readline()
-		while 'header' not in line:
-			line = f.readline()
-
-		for line in csv.DictReader(f, dialect='excel-tab'):
-			cds = line['type']
-			if cds is 'CDS':
-				continue
-			
-			strand = line['strand']
-
-			if strand is 'R':
-				startpos = line['end_pos']
-				stoppos = line['start_pos']
-			else: # D or DR
-				startpos = line['start_pos']
-				stoppos = line['end_pos']
-
-			cdsstarts.append(startpos)
-			cdsstops.append(stoppos)
-			cdsstrands.append(strand)
-
-
-	f.close()
-
-	badmotifs = {}
-
-	for bc in fimodict: # So... there are multiple motifs in a bicluster fimo file
-                print bc
-		fimomotifs = fimodict[bc]
-		badmotifsset = set()
-
-		for fimomotif in fimomotifs:
-
-			info = fimomotifs[fimomotif]
-			#fimostart = int(info[0])
-			fimostop = int(info[0])
-			fimostrand = info[1]
-
-			# If fimostrand is '+' look up fimo stop in cdsstarts to see if it is greater (and then check too it is less than cdsstop)
-			# If fimostrand is '-' look up fimo stop in cdsstarts to see if it is less than (and also greater than cdsstop)
-
-			for i in range(0, len(cdsstarts)):
-
-				if (fimostrand is '+' and cdsstrands[i] is 'D') or \
-						(fimostrand is '-' and cdsstrands[i] is 'R'):
-
-					if (fimostop >= int(cdsstarts[i]) and fimostop <= int(cdsstops[i])):
-						badmotifsset.add(fimomotif) # within CDS or overlapping at beginning of CDS
-
-		badmotifs[bc] = badmotifsset
-		#frac = 1.0 - (float(len(badmotifs[bc]))/float(len(fimodict[bc])))
-		#print frac
-
-	#  Print the fimo file name and the resulting fraction of goodmotifs to all motifs
-        # NO THIS IS WRONG - should have separate fractions for each of the 2 motifs in each fimo file!!!
-
-	OUT = open(os.path.join(opt.input_dir,'coding_fracs.tsv'), 'wb')
-
-	for bc in fimodict:
-		if len(fimodict[bc]) > 0:
-			frac = 1.0 - (float(len(badmotifs[bc]))/float(len(fimodict[bc])))
-			OUT.write(bc+'\t'+str(frac)+'\n')
-		else:
-			OUT.write(bc+'\tNA\n')
-
-	OUT.close()
+        fimo['in_coding_rgn'] = is_bad
+        fimo.to_csv( bz2.BZ2File( f, 'w' ), sep='\t', index=False )
 
 if __name__ == '__main__':
-	start_time = float(time.time())
-	main()
-	print "--- %f seconds ---" % (float(time.time()) - start_time)
+    main()
