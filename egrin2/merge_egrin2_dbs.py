17cb75ebf692992eaeddca7bb1e56c8f8dc5d3d2
Aaron Brooks <abrooks@systemsbiology.org>
2014-12-03 16:04:04 -0800

    assembling bicluster info

--- egrin2/merge_egrin2_dbs.py
+++ egrin2/merge_egrin2_dbs.py
@@ -26,7 +26,8 @@ import numpy as np
 import pandas as pd
 
 import sqlite3
-from pymongo import MongoClient
+import pymongo
+from pymongo im%pasteport MongoClient
 import gridfs
 
 # ratios_raw = "/Users/abrooks/Desktop/Active/Eco_ensemble_python_m3d/ratios_eco_m3d.tsv.gz"
@@ -34,137 +35,255 @@ import gridfs
 # >>> st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
 # row_annot = "/Users/abrooks/Desktop/Active/Eco_ensemble_python_m3d/genomeInfo.tsv.gz"
 # col_annot = "/Users/abrooks/Desktop/Active/Eco_ensemble_python_m3d/E_coli_v4_Build_6.experiment_feature_descriptions.tsv.gz"
+# db_file = "/Users/abrooks/Desktop/Active/Eco_ensemble_python_m3d/eco-ens-m3d/eco-out-001/cmonkey_run.db"
 
 class sql2mongoDB:
     
-    def __init__( self, e_dir = None, prefix = None, gene_info = None, ratios_raw = None, col_annot = None, row_annot = None, dbname = None ):
-    	
-    	# connect to database
-	# make sure mongodb is running
-	# retvalue = os.system("nohup mongod --port 27017 --quiet &")
-	try:
-    		client = MongoClient('mongodb://localhost:27017/') 
-    		print "Connected to MongoDB"
-	except pymongo.errors.ConnectionFailure, e:
-   		print "Could not connect to MongoDB: %s" % e
+	def __init__( self, e_dir = None, prefix = None, gene_info = None, ratios_raw = None, col_annot = None, row_annot = None, dbname = None ):
+		
+		# connect to database
+		# make sure mongodb is running
+		retvalue = os.system("nohup mongod --port 27017 --quiet &")
+		try:
+			client = MongoClient('mongodb://localhost:27017/') 
+			print "Connected to MongoDB"
+		except pymongo.errors.ConnectionFailure, e:
+			print "Could not connect to MongoDB: %s" % e
 
-	if dbname = None:
-		dbname = "egrin2_db"
-	if dbname in client.database_names():
-		print "WARNING: %s database already exists!!! You are probably just duplicating the data. I don't protect you from this currently." % dbname
-	else:
-		print "Initializing MongoDB database: %s" % dbname
-	db = client[dbname]
+		if dbname == None:
+			dbname = "egrin2_db"
+		if dbname in client.database_names():
+			print "WARNING: %s database already exists!!! You are probably just duplicating the data. I don't protect you from this currently." % dbname
+		else:
+			print "Initializing MongoDB database: %s" % dbname
+		db = client[dbname]
 
-    	# get db files in directory
-    	if prefix is None:
-    		prefix = 'eco-out-'
-    	else:
-        		prefix = prefix
-        	if e_dir is None:
-    		e_dir = './eco-ens-m3d/'
-    	else:
-        		prefix = prefix
-        	self.db_files = np.sort( np.array( glob.glob( e_dir + prefix + "???/cmonkey_run.db" ) ) ) # get all cmonkey_run.db files
-        	self.ratios = loadRatios(self, ratios_raw)
-        	self.ratios_standardized = standardizeRatios(self, self.ratios)
-        	
-        	#####################
-        	# load additional row info
-        	#####################
-        	if  row_annot != None:
-        		# assumes default microbes online schema
-        		row_annot = pd.read_csv( gzip.open( row_annot, 'rb' ), sep="\t" )	
-        	# make row2id and id2row dicts for lookup
-        	rows = self.ratios_standardized.index.values
-        	rows.sort()
-        	self.row_info =  pd.DataFrame( zip( range( len( rows ) ), rows ), index = rows, columns = [ "row_id", "egrin2_row_name"] )
-        	# join with row_annot
-        	row_table = pd.merge( self.row_info, row_annot, left_on="egrin2_row_name", right_on="sysName" )
-        	# write to mongoDB collection 
-        	print "Inserting to row_info collection"
-        	row_info_collection = db.row_info
-        	row_info_collection.insert( row_table.to_dict( outtype='records' ) )
-        	#
-        	# example queries
-        	#
-        	# for i in row_info_collection.find( { "name" : "carA" } ):
-        	# 	print i
-        	# for i in row_info_collection.find( { "sysName": { "$in" : ["b0032", "b0124", "b0089", "b0432","b2234","b0456"] } } ):
-        	# 	print "%s = %s" % ( i["sysName"], i["name"] )
-        	# 	print "It does: %s" % i["GO"]
-        	# for i in row_info_collection.find( { "GO": { "$regex" : "GO:0006541," } } ):
-        	# 	print i["sysName"]
-        	# 	print i["GO"]
+		# get db files in directory
+		if prefix is None:
+			prefix = 'eco-out-'
+		else:
+	    		prefix = prefix
+	    	if e_dir is None:
+			e_dir = './eco-ens-m3d/'
+		else:
+	    		prefix = prefix
+	    	self.db_files = np.sort( np.array( glob.glob( e_dir + prefix + "???/cmonkey_run.db" ) ) ) # get all cmonkey_run.db files
+	    	self.db_files = checkRuns( self, self.db_files )
+	    	self.run2id = get_run2id(self, self.db_files )
+	    	self.ratios = loadRatios(self, ratios_raw)
+	    	print "Standardizing gene expression..."
+	    	self.ratios_standardized = standardizeRatios(self, self.ratios)
+	    	print "Inserting into row_info collection"
+	    	self.row2id = get_row2id( self, self.ratios_standardized )
+	    	self.row_info_collection = insert_row_info( self, self.row2id, row_annot )
+	    	print "Inserting into col_info collection"
+	    	self.col2id = get_col2id( self, self.ratios_standardized )
+	    	self.col_info_collection = insert_col_info( self, self.col2id, col_annot )
+	    	print "Inserting into ensemble_info collection"
+	    	self.ensemble_info_collection = insert_ensemble_info( self, self.db_files, self.run2id, self.row2id, self.col2id )
 
-        	#####################
-        	# load additional col info
-        	#####################
-        	if  col_annot != None:
-        		# assumes default microbes online schema
-        		col_annot = pd.read_csv( gzip.open( col_annot, 'rb' ), sep="\t" )	
-        	# make cond2id and id2cond dicts for lookup
-        	print "Inserting to row_info collection"
-        	cols = self.ratios_standardized.columns.values
-        	cols.sort()
-        	self.col_info =  pd.DataFrame( zip( range( len( cols ) ), cols ), columns = [ "col_id", "egrin2_col_name"] )
-        	col_table = pd.merge( self.col_info, col_annot, left_on="egrin2_col_name", right_on="experiment_name" )
-        	col_info_4_mongoDB = []
-        	for i in range( 0,len( col_info.egrin2_col_name ) ):
-        		col_info_4_mongoDB.append( condInfo2Dict( 0, col_table, col_info.egrin2_col_name[i] ) )
-        	col_info_collection = db.col_info
-        	col_info_collection.insert( col_info_4_mongoDB )
-        	# for i in col_info_collection.find( { "additional_info.name": "growth_phase", "additional_info.value": "biofilm"  } ):
-        	# 	print i["egrin2_col_name"]
-        	# for i in col_info_collection.find( { "$and": [ { "additional_info.name": "strain", "additional_info.value":  { "$regex": "MG1655" } }, { "additional_info.name": "growth_phase", "additional_info.value": "biofilm"  } ] } ):
-        	# 	print i
-        	# for i in col_info_collection.find( { "$and": [ { "additional_info.value":   "0.0000303"  }, { "additional_info.name": "growth_phase", "additional_info.value": "biofilm"  } ] } ):
-        	# 	print i["egrin2_col_name"]
+    	def checkRuns( self, db_files ):
+		"""make sure the runs have data!!!"""
+		to_keep = []
+	    	for i in db_files:
+	    		conn = sqlite3.connect( i )
+		    	c = conn.cursor()
+		    	c.execute("SELECT * FROM run_infos;")
+		    	run_info = c.fetchone()
+	    		if run_info is None:
+	    			pass
+	    		else:
+	    			to_keep.append( i )
+	    	return to_keep
 
-    def loadRatios(self, file_in):
-    	"""Loads ratios from individual cMonkey runs (unfinished) or single flat file (gzip compressed)."""
-        	if file_in = None:
-        		# compile from individual runs
-        		# do be done
-        		file_in = np.sort( np.array( glob.glob( e_dir + prefix + "???/ratios.tsv.gz" ) ) )
-        	else:
-        		print "Loading gene expression file from %s" % ratios_raw
-        		# load directly from gzip file
-        		ratios= pd.read_csv( gzip.open( file_in, 'rb' ), index_col=0, sep="\t" ) 
-        	return ratios
+    	def get_run2id( self, dbfiles ):
+		"""make run2id"""
+		runs = []
+	    	for i in dbfiles:
+	    		runs.append(i.split("/")[-2])
+	    	row_info =  pd.DataFrame( zip( range( len( runs ) ), runs ), index = runs, columns = [ "run_id", "run_name"] )
+	    	return row_info
+	
+	def loadRatios( self, file_in ):
+		"""Loads ratios from individual cMonkey runs (unfinished) or single flat file (gzip compressed)."""
+		if file_in == None:
+			# compile from individual runs
+			# do be done
+			file_in = np.sort( np.array( glob.glob( e_dir + prefix + "???/ratios.tsv.gz" ) ) )
+		else:
+			print "Loading gene expression file from %s" % ratios_raw
+			# load directly from gzip file
+			ratios= pd.read_csv( gzip.open( file_in, 'rb' ), index_col=0, sep="\t" ) 
+		return ratios
 
-     def standardizeRatios(self, ratios):
-     	"""compute standardized ratios (global). row standardized"""
-        	ratios_standardized = ratios.copy()
-	zscore = lambda x: ( x - x.mean() ) / x.std()
-	print "Standardizing gene expression..."
-	for row in ratios.iterrows():
-		ratios_standardized.loc[ row[0] ] = zscore( row[1] )
-	return ratios_standardized
-        	
-    def condInfo2Dict( self, col_table, cond_name):
-    	cond_data = col_table[col_table.egrin2_col_name==cond_name]
-    	cond_dict = { "col_id": np.unique( cond_data.col_id )[0], "egrin2_col_name": np.unique( cond_data.egrin2_col_name )[0], "additional_info": [] }
-    	for i in range(0,cond_data.shape[0]):
-    		cond_dict[ "additional_info" ].append( { "name": cond_data.irow(i)["feature_name" ], "value": cond_data.irow(i)["value"], "units": cond_data.irow(i)["feature_units"] } )
-    	return cond_dict
+	def standardizeRatios( self, ratios ):
+		"""compute standardized ratios (global). row standardized"""
+		ratios_standardized = ratios.copy()
+		zscore = lambda x: ( x - x.mean() ) / x.std()
+		for row in ratios.iterrows():
+			ratios_standardized.loc[ row[0] ] = zscore( row[1] )
+		return ratios_standardized
 
-    def parseRatios( self, ratios_files ):
-    	"""Compile ratios from available runs. Note that each run stores only the ratios that were used for that run."""
-    	## unnecessary at the moment since each of the standardized ratios are different at the moment
-    	# i = 0
-    	# while i < len(ratios_files):
-    	# 	if i == 0:
-    	# 		rats_df = pd.read_csv( gzip.open( ratios_files[i], 'rb' ), index_col=0, sep="\t" ) 
-    	# 	else:
-    	# 		new_df = pd.read_csv( gzip.open( ratios_files[i], 'rb' ), index_col=0, sep="\t" )
-    	# 		rats_df = rats_df + new_df
-    	# 	i = i+1 
+	def get_row2id( self, ratios_standardized ):
+		"""make row2id and id2row dicts for lookup"""
+	    	rows = ratios_standardized.index.values
+	    	rows.sort()
+	    	row_info =  pd.DataFrame( zip( range( len( rows ) ), rows ), index = rows, columns = [ "row_id", "egrin2_row_name"] )
+	    	return row_info
 
-    def parseSQLite( self, db_file ):
-    	"""Create python dictionaries for bulk import into MongoDB collections"""
-        	conn = sqlite3.connect(db_file)
-        	c = conn.cursor()
+	def insert_row_info( self, row_info, row_annot, left_on = None, right_on = None ):
+		"""
+		Insert row_info into mongoDB database
+
+
+	    	example queries
+	    	------------------------------
+	    	for i in row_info_collection.find( { "name" : "carA" } ):
+	    		print i
+	    	for i in row_info_collection.find( { "sysName": { "$in" : ["b0032", "b0124", "b0089", "b0432","b2234","b0456"] } } ):
+	    		print "%s = %s" % ( i["sysName"], i["name"] )
+	    		print "It does: %s" % i["GO"]
+	    	for i in row_info_collection.find( { "GO": { "$regex" : "GO:0006541," } } ):
+	    		print i["sysName"]
+	    		print i["GO"]
+
+		"""
+	    	# load additional row info
+	    	if  row_annot != None:
+	    		# assumes default microbes online schema
+	    		row_annot = pd.read_csv( gzip.open( row_annot, 'rb' ), sep="\t" )	
+	    	if left_on == None:
+	    		left_on="egrin2_row_name"
+	    	if right_on == None:
+	    		right_on="sysName"
+	    	# join with row_annot
+	    	row_table = pd.merge( self.row_info, row_annot, left_on=left_on, right_on=right_on )
+	    	# write to mongoDB collection 
+	    	
+	    	row_info_collection = db.row_info
+	    	# TODO:
+	    	# Check whether documents are already present in the collection before insertion
+	    	# In case where they are present, update them
+	    	row_info_collection.insert( row_table.to_dict( outtype='records' ) )
+
+	    	return row_info_collection
+
+	def get_col2id( self, ratios_standardized ):
+		"""make cond2id and id2cond dicts for lookup"""
+	    	cols = ratios_standardized.columns.values
+	    	cols.sort()
+	    	col_info =  pd.DataFrame( zip( range( len( cols ) ), cols ), index = cols, columns = [ "col_id", "egrin2_col_name"] )
+	    	return col_info
+
+    	def insert_col_info( self, col_info, col_annot ):
+		"""
+		Insert col_info into mongoDB database
+
+
+	    	example queries
+	    	------------------------------
+	    	for i in col_info_collection.find( { "additional_info.name": "growth_phase", "additional_info.value": "biofilm"  } ):
+	    		print i["egrin2_col_name"]
+	    	for i in col_info_collection.find( { "$and": [ { "additional_info.name": "strain", "additional_info.value":  { "$regex": "MG1655" } }, { "additional_info.name": "growth_phase", "additional_info.value": "biofilm"  } ] } ):
+	    		print i["egrin2_col_name"]
+	    	for i in col_info_collection.find( { "$and": [ { "additional_info.value":   "0.0000303"  }, { "additional_info.name": "growth_phase", "additional_info.value": "biofilm"  } ] } ):
+	    		print i["egrin2_col_name"]
+
+		"""
+	    	# load additional col info
+	    	
+	    	if  col_annot != None:
+	    		# assumes default microbes online schema
+	    		col_annot = pd.read_csv( gzip.open( col_annot, 'rb' ), sep="\t" )	
+	    	
+	    	col_table = pd.merge( self.col_info, col_annot, left_on="egrin2_col_name", right_on="experiment_name" )
+	    	col_info_4_mongoDB = []
+	    	for i in range( 0, len( col_info.egrin2_col_name ) ):
+	    		col_info_4_mongoDB.append( condInfo2Dict( 0, col_table, col_info.egrin2_col_name[i] ) )
+	    	col_info_collection = db.col_info
+	    	# TODO:
+	    	# Check whether documents are already present in the collection before insertion
+	    	# In case where they are present, update them
+	    	col_info_collection.insert( col_info_4_mongoDB )
+
+	    	return col_info_collection
+	    	 	
+	def condInfo2Dict( self, col_table, cond_name):
+		cond_data = col_table[col_table.egrin2_col_name==cond_name]
+		cond_dict = { "col_id": np.unique( cond_data.col_id )[0], "egrin2_col_name": np.unique( cond_data.egrin2_col_name )[0], "additional_info": [] }
+		for i in range(0,cond_data.shape[0]):
+			cond_dict[ "additional_info" ].append( { "name": cond_data.irow(i)["feature_name" ], "value": cond_data.irow(i)["value"], "units": cond_data.irow(i)["feature_units"] } )
+		return cond_dict
+
+	def parseRatios( self, ratios_files ):
+		"""Compile ratios from available runs. Note that each run stores only the ratios that were used for that run."""
+		## unnecessary at the moment since each of the standardized ratios are different at the moment
+		# i = 0
+		# while i < len(ratios_files):
+		# 	if i == 0:
+		# 		rats_df = pd.read_csv( gzip.open( ratios_files[i], 'rb' ), index_col=0, sep="\t" ) 
+		# 	else:
+		# 		new_df = pd.read_csv( gzip.open( ratios_files[i], 'rb' ), index_col=0, sep="\t" )
+		# 		rats_df = rats_df + new_df
+		# 	i = i+1 
+
+	def assemble_ensemble_info( self, db_file, run2id, row2id, col2id ):
+		"""Create python ensemble_info dictionary for bulk import into MongoDB collections"""
+		run_name = db_file.split("/")[-2]
+		conn = sqlite3.connect(db_file)
+	    	c = conn.cursor()
+	    	c.execute("SELECT start_time, finish_time, num_iterations, organism, species, num_rows, num_columns, num_clusters, git_sha FROM run_infos;")
+	    	run_info = c.fetchone()
+	    	c.execute("SELECT name FROM row_names;")
+	    	rows = [ row2id.loc[ str(i[0]) ].row_id for i in c.fetchall() ]
+	    	c.execute("SELECT name FROM column_names;")
+	    	cols = [ col2id.loc[ str(i[0]) ].col_id for i in c.fetchall() ]
+	    	d = {
+	    	"run_id": run2id.loc[run_name].run_id,
+	    	"run_name": run_name,
+	    	"start_time": str( run_info[0] ),
+	    	"finish_time": str( run_info[1] ),
+	    	"num_iterations": int( run_info[2] ),
+	    	"organism": str( run_info[3] ),
+	    	"species": str( run_info[4] ),
+	    	"num_rows": int( run_info[5] ),
+	    	"rows": rows,
+	    	"num_columns": int( run_info[6] ),
+	    	"cols": cols,
+	    	"num_clusters": int( run_info[7] ),
+	    	"git_sha": str( run_info[8] ),
+	    	"added_to_ensemble": datetime.datetime.utcnow()
+	    	}
+	    	conn.close()
+	    	return d
+
+
+	def insert_ensemble_info( self, db_files, run2id, row2id, col2id ):
+		"""Compile and insert ensemble_info collection into MongoDB collection"""
+	    	to_insert = [ assemble_ensemble_info( self, i, run2id, row2id, col2id ) for i in db_files ]
+	    	ensemble_info_collection = db.ensemble_info
+	    	# TODO:
+	    	# Check whether documents are already present in the collection before insertion
+	    	# In case where they are present, update them
+	    	ensemble_info_collection.insert( to_insert )
+	    	return ensemble_info_collection
+
+	def assemble_bicluster_info_single( self, db_file, cluster, run2id, row2id, col2id ):
+		"""Create python ensemble_info dictionary for bulk import into MongoDB collections"""
+		run_name = db_file.split("/")[-2]
+		conn = sqlite3.connect(db_file)
+	    	c = conn.cursor()
+	    	c.execute("SELECT max(iteration) FROM cluster_stats;")
+	    	last_run = c.fetchone()[0] # i think there is an indexing problem in cMonkey python!! 
+	    	w = (cluster,last_run)
+	    	c.execute("SELECT residual FROM cluster_stats WHERE cluster = ? AND iteration = ?;", w )
+	    	d = {
+	    	"run_id": run2id.loc[run_name].run_id,
+	    	"rows":,
+	    	"columns":,
+	    	"residual":,
+	    	"motif":
+	    	}
 
 
 
