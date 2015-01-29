ea36b39f154db1e727aaa4b3cf47503ccc367fc4
Aaron Brooks <abrooks@systemsbiology.org>
2015-01-28 17:31:15 -0800

    working agglom

--- query/egrin2_query.py
+++ query/egrin2_query.py
@@ -89,22 +89,15 @@ def col2id( col, host, port, db,  verbose = False, return_field = "row_id" ):
 		print "ERROR: Cannot identify col name: %s" % col
 		return None
 
-def col2name( col, host, port, db,  verbose = False ):
+
+def col2id_batch( cols, host, port, db,  verbose = False, return_field = "col_id" ):
 	"""Check name format of rows. If necessary, translate."""
 	client = MongoClient( 'mongodb://'+host+':'+str(port)+'/' )
-	query = list( client[db].col_info.find( { "$or": [ { "col_id": col }, { "egrin2_col_name": col } ] } ) )
+	query = pd.DataFrame( list( client[db].col_info.find( { "$or": [ { "col_id": { "$in": cols } }, { "egrin2_col_name": { "$in":cols } } ] }, { return_field: 1 } ) ) ).loc[ :, return_field ].tolist()
 	client.close()
-	if len( query ) == 1: 
-		col = query[ 0 ][ "egrin2_col_name" ]
-		return col
-	elif len( query ) > 0:
-		print "ERROR: Multiple genes match the row name: %s" % col
-		if verbose:
-			print query
-		return None
-	else:
-		print "ERROR: Cannot identify row name: %s" % col
-		return None
+	if len( cols ) > len( query ): 
+		print "WARNING: Returning fewer rows than originally supplied"
+	return query
 
 def colResamplePval( rows = None, cols = None, n_resamples = None, host = "localhost", port = 27017, db = "", standardized = None, sig_cutoff = None, sort = True, add_override = False, n_jobs = 4, keepP = 0.1, verbose = True ):
 
@@ -239,11 +232,11 @@ def rows2corem( rows = [ 0, 1 ], host = "localhost", port = 27017, db = "",  ver
 		print "Could not find any corems matching your criteria"
 		return None
 
-def x2bicluster( x = [ 0,1 ], x_type = None, logic = "and", count = True, host = "localhost", port = 27017, db = "",  verbose = False, return_field = [ "cluster" ] ):
+def agglom( x = [ 0,1 ], x_type = None, y_type = None, logic = "and", host = "localhost", port = 27017, db = "",  verbose = False, gre_lim = 10, pval_cutoff = 0.05, translate = True ):
 	"""
 	Determine how often 'x' occurs in biclusters. Usually just retrieve the counts. Retrieve additional bicluster info by setting count to False
 
-	Available x_type(s):
+	Available x_type(s)/y_type(s):
 
 	'rows' or 'genes': search for row (genes) in biclusters. x should be a list of rows, eg ["carA","carB"] or [275, 276]
 	'columns' or 'conditions': search for columns (conditions) in biclusters. x should be a list of columns, eg ["dinI_U_N0025", "dinP_U_N0025"] or [0,1]
@@ -252,12 +245,31 @@ def x2bicluster( x = [ 0,1 ], x_type = None, logic = "and", count = True, host =
 
 	"""
 
+	def compute_p( i, M, N ):
+		#print i
+		z = i.counts # n black balls in draw
+		n = i.all_counts # num black balls tot
+		M = M
+		#M = M - n  # n white balls
+		N = N # num drawn
+		prb =  hypergeom.sf(z, M, n, N)
+		return prb
+
 	if x_type is None:
-		print "Please supply a type for your query. Types include: 'rows' (genes), 'columns' (conditions), 'gres' "
+		print "Please supply an x_type for your query. Types include: 'rows' (genes), 'columns' (conditions), 'gres' "
+	if y_type is None:
+		print "Please supply an y_type for your query. Types include: 'rows' (genes), 'columns' (conditions), 'gres'. Biclusters will be returned by default. "
+		y_type = "cluster"
+
+	if type( x ) == str or type( x ) == int:
+		# single
+		x = [ x ]
 
 	client = MongoClient( 'mongodb://'+host+':'+str(port)+'/' )
 
-	if x_type == "rows" or "row" or "gene" or "genes":
+	# Check input types
+
+	if x_type == "rows" or x_type == "row" or x_type == "gene" or x_type == "genes":
 		x_type = "rows"
 		x_o = x
 		x = [ row2id( i, host, port, db ) for i in x ]
@@ -267,7 +279,7 @@ def x2bicluster( x = [ 0,1 ], x_type = None, logic = "and", count = True, host =
 			print "Cannot translate row names: %s" % x_o
 			return None
 
-	if x_type == "columns" or "column" or "condition" or "conditions":
+	if x_type == "columns" or x_type == "column" or x_type == "col" or x_type == "cols" or x_type == "condition" or x_type == "conditions":
 		x_type = "columns"
 		x_o = x
 		x = [ col2id( i, host, port, db ) for i in x ]
@@ -277,105 +289,120 @@ def x2bicluster( x = [ 0,1 ], x_type = None, logic = "and", count = True, host =
 			print "Cannot translate col names: %s" % x_o
 			return None  
 
-	if x_type == "motif" or "gre" or "motc" or "motif.gre" or "motfs" or "gres" or "motcs":
+	if x_type == "motif" or x_type == "gre" or x_type == "motc" or x_type == "motif.gre" or x_type == "motfs" or x_type == "gres" or x_type == "motcs":
 		x_type = "motif.gre_id"
 
-	if x_type in [ "rows", "columns", "motif.gre_id" ] and logic in [ "and","or","nor" ]:
+	if x_type == "cluster" or x_type == "clusters" or x_type == "bicluster" or x_type == "biclusters":
+		print "WARNING! I hope you are using cluster _id!!! Otherwise the results might surprise you..."
+		x_type = "_id"
+
+	# Check output types
+
+	if y_type == "rows" or y_type == "row" or y_type == "gene" or y_type == "genes":
+		y_type = "rows"
+
+	if y_type == "columns" or y_type == "column" or y_type == "col" or y_type == "cols" or y_type == "condition" or y_type == "conditions":
+		y_type = "columns"
+		
+	if y_type == "motif" or y_type == "gre" or y_type == "motc" or y_type == "motif.gre" or y_type == "motfs" or y_type == "gres" or y_type == "motcs":
+		y_type = "motif.gre_id"
+
+	if y_type == "cluster" or y_type == "clusters" or y_type == "bicluster" or y_type == "biclusters":
+		print "WARNING! Will return bicluster _id. The results might surprise you..."
+		y_type = "_id"
+
+	# Compose query
+
+	if logic in [ "and","or","nor" ]:
 		q = { "$"+logic: [ { x_type : i } for i in x ] }
-		if count:
-			query =client[db].bicluster_info.find( q ).count()
-			return query
-		else:
-			query = pd.DataFrame( list( client[db].bicluster_info.find( q ) ) )
-	else:
-		print "I don't recognize the logic you are trying to use. 'logic' must be 'and', 'or', or 'nor'."
-	
-	client.close()
-
-	if query.shape[0] > 0: 
-		if return_field == "all":
-			return query
-		else:
-			try:
-				return query.loc[ :, return_field ]
-			except Exception:
-				return query
-	else:
-		print "Could not find any biclusters matching your criteria"
-		return None
-
-def bicluster2x( bicluster_ids = [ ], x_type = None, host = "localhost", port = 27017, db = "",  verbose = False ):
-	"""
-	Determine how often 'x' occurs in biclusters. Usually just retrieve the counts. 
-
-	Available x_type(s):
-
-	'rows' or 'genes': search for row (genes) in biclusters. x should be a list of rows, eg ["carA","carB"] or [275, 276]
-	'columns' or 'conditions': search for columns (conditions) in biclusters. x should be a list of columns, eg ["dinI_U_N0025", "dinP_U_N0025"] or [0,1]
-	'gre': search for GREs in biclusters. x should be a list of GRE IDs, eg [4, 19]
-	''
-
-	"""
-	if x_type is None:
-		print "Please supply a type for your query. Types include: 'rows' (genes), 'columns' (conditions), 'gres' "
-
-	client = MongoClient( 'mongodb://'+host+':'+str(port)+'/' )
-
-	if x_type == "rows" or "row" or "gene" or "genes":
-		x_type = "rows"
-
-	if x_type == "columns" or "column" or "condition" or "conditions":
-		x_type = "columns"
-
-	if x_type == "motif" or "gre" or "motc" or "motif.gre" or "motfs" or "gres" or "motcs":
-		x_type = "motif.gre_id"
-
-	if x_type in [ "rows", "columns", "motif.gre_id" ] and logic in [ "and","or","nor" ]:
-		q = { "_id": { "$in": bicluster_ids } }
-		o = { x_type: 1 }
+		o = { y_type: 1 }
 		query = pd.DataFrame( list( client[db].bicluster_info.find( q, o ) ) )
 	else:
 		print "I don't recognize the logic you are trying to use. 'logic' must be 'and', 'or', or 'nor'."
+		return None
 	
 	client.close()
 
 	if query.shape[0] > 0: 
-		if x_type == "rows":
-			in_db = pd.DataFrame( list( client[db].row_info.find( { }, { "_id" : 0, "row_id": 1 } ) ) ).row_id.tolist()
+		if y_type == "rows":
 			rows = pd.Series( list( itertools.chain( *query.rows.tolist() ) ) ).value_counts().to_frame( "counts" )
+			
 			# filter out rows that aren't in the database - i.e. not annotated in MicrobesOnline
+			in_db = pd.DataFrame( list( client[db].row_info.find( { }, { "_id" : 0, "row_id": 1 } ) ) ).row_id.tolist()
 			common_rows = list(set(rows.index).intersection(set(in_db)))
 			rows = rows.loc[common_rows]
+			
+			# find all bicluster counts
+			all_counts = pd.DataFrame( list( client[db].bicluster_info.find( { }, { "rows": 1 } ) ) )
+			all_counts = pd.Series( list( itertools.chain( *all_counts.rows.tolist() ) ) ).value_counts().to_frame( "all_counts" )
+			# combine two data frames
+			to_r = rows.join(all_counts).sort("counts",ascending=False)
 
-			rows[ "pval" ] = 
-			t_rows = [ row2id( i, host, port, db,  verbose = False, return_field = "egrin2_row_name" ) for i in rows.index ]
-			rows.index = 
-		
-		if return_field == "all":
-			return query
-		else:
-			try:
-				return query.loc[ :, return_field ]
-			except Exception:
-				return query
+			if translate:
+				to_r.index = row2id_batch( to_r.index.tolist(), host, port, db, return_field = "egrin2_row_name" )
+
+		if y_type == "columns":
+			cols = pd.Series( list( itertools.chain( *query["columns"].tolist() ) ) ).value_counts().to_frame( "counts" )
+			
+			# filter out rows that aren't in the database - i.e. not annotated in MicrobesOnline
+			in_db = pd.DataFrame( list( client[db].col_info.find( { }, { "_id" : 0, "col_id": 1 } ) ) ).col_id.tolist()
+			common_cols = list( set( cols.index ).intersection( set( in_db )  ))
+			cols = cols.loc[common_cols]
+			
+			# find all bicluster counts
+			all_counts = pd.DataFrame( list( client[db].bicluster_info.find( { }, { "columns": 1 } ) ) )
+			all_counts = pd.Series( list( itertools.chain( *all_counts["columns"].tolist() ) ) ).value_counts().to_frame( "all_counts" )
+			# combine two data frames
+			to_r = cols.join(all_counts).sort("counts",ascending=False)
+
+			if translate:
+				to_r.index = col2id_batch( to_r.index.tolist(), host, port, db, return_field = "egrin2_col_name" )
+
+
+		if y_type == "motif.gre_id":
+			gres = pd.Series( list( itertools.chain( *[ i.values() for i in list( itertools.chain( *query.motif.tolist() ) ) ] ) ) )
+			gres = gres.value_counts().to_frame( "counts" )
+
+			# find all bicluster counts
+			all_counts = pd.DataFrame( list( client[db].bicluster_info.find( { }, { "motif.gre_id": 1 } ) ) )
+			all_counts = pd.Series( list( itertools.chain( *[ i.values() for i in list( itertools.chain( *all_counts.motif.tolist() ) ) ] ) ) ).value_counts().to_frame( "all_counts" )
+			# combine two data frames
+			to_r = gres.join(all_counts).sort("counts",ascending=False)
+
+			# filter by GREs with more than 10 instances
+			to_r = to_r.loc[ to_r.all_counts>=gre_lim, : ]
+
+
+		to_r["pval"] = to_r.apply( compute_p, axis=1, M = client[db].bicluster_info.count(),  N = query.shape[ 0 ] )
+		to_r["qval_BH"] = multipletests( to_r.pval, method='fdr_bh' )[1]
+		to_r["qval_bonferroni"] = multipletests( to_r.pval, method='bonferroni' )[1]
+		to_r = to_r.sort( "pval", ascending=True )
+		# only return below pval cutoff
+		to_r = to_r.loc[ to_r.pval <= pval_cutoff, : ]
+
+		return to_r
+	
 	else:
+		
 		print "Could not find any biclusters matching your criteria"
 		return None
 
-	return None
-
-def agglom( x, x_type, out_type, path ):
-	return None
-
-
-
-	
-	
-	
-	
 
 if __name__ == '__main__':
+	print "yuuuup"
+	# host = "primordial"
+	# port = 27017
+	# db = "egrin2_db"
 
-	host = "primordial"
-	port = 27017
-	db = "egrin2_db"
+	client = MongoClient( 'mongodb://'+host+':'+str(port)+'/' )
+	# test rows
+	corem = client[db].corem.find_one({"corem_id":1})["rows"]
+	# bicluster_ids = x2bicluster( x = corem, x_type = "rows", logic = "or", count = False, host = host, port = port, db = db,  verbose = False, return_field = [ "_id" ] )._id.tolist()
+
+	# # test cols
+	# cols = range(5)
+	# bicluster_ids = x2bicluster( x = cols, x_type = "cols", logic = "or", count = False, host = host, port = port, db = db,  verbose = False, return_field = [ "_id" ] )._id.tolist()
+
+	# # test gres
+	# gre = [2]
+	# bicluster_ids = x2bicluster( x = gre, x_type = "gre", logic = "or", count = False, host = host, port = port, db = db,  verbose = False, return_field = [ "_id" ] )._id.tolist()
