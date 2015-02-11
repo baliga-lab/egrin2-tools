5b36fee68d074d8817f32db436bdbf964c81ea87
Aaron Brooks <abrooks@systemsbiology.org>
2015-02-11 11:25:06 -0800

    expression plots

--- query/egrin2_query.py
+++ query/egrin2_query.py
@@ -574,19 +574,11 @@ def expressionFinder( rows = None, cols = None, standardized = True, host = "loc
 
 	return data
 
-def plotExpression( data, plot_type = "line", ipynb = False, zlim = None ):
+def plotExpression( data, plot_type = "boxplot", ipynb = False, zlim = None, sort = True, boxpoints = None ):
 	
-	if zlim is None:
-		# use 1st and 99th percentile
-		all_data = list( itertools.chain( *data.values.tolist() ) )
-		if np.percentile( all_data, 1 ) < 0:
-			zmin = -max( abs( np.percentile( all_data, [ 1, 99 ] ) ) )
-		else:
-			zmin = np.percentile( all_data, 1 )
-		zmax = max( abs( np.percentile( all_data, [ 1, 99 ] ) ) )
-	else:
-		zmin = zlim[0]
-		zmax = zlim[1]
+	if sort:
+		c_order = data.mean(0).order().index.tolist()
+		data = data.loc[:,c_order]
 
 	def to_scatter( df ):
 		x = df.index.values
@@ -601,18 +593,53 @@ def plotExpression( data, plot_type = "line", ipynb = False, zlim = None ):
 		lines_plotly=[lines[key] for key in df]
 		return lines_plotly
 
-	def to_heatmap( df ):
+	def to_box( df, boxpoints ):
+
+		if boxpoints:
+			boxpoints = "all"
+		elif df.shape[1]<=50 and boxpoints is None:
+			boxpoints = "all"
+		elif boxpoints is None:
+			boxpoints = False
+		else:
+			boxpoints = boxpoints
+
+		boxes = []
+		for x in df.columns.tolist():
+			boxes.append(Box(
+					name = x,
+					y = df.loc[:,x].tolist(),
+					boxpoints=boxpoints,
+        			jitter=0.3,
+        			pointpos=0
+				))
+		return boxes
+
+	def to_heatmap( df, zlim ):
+
+		if zlim is None:
+			# use 1st and 99th percentile
+			all_data = list( itertools.chain( *df.values.tolist() ) )
+			if np.percentile( all_data, 1 ) < 0:
+				zmin = -max( np.abs( np.percentile( all_data, [ 1, 99 ] ) ) )
+			else:
+				zmin = np.percentile( all_data, 1 )
+			zmax = max( np.abs( np.percentile( all_data, [ 1, 99 ] ) ) )
+		else:
+			zmin = zlim[0]
+			zmax = zlim[1]
+
 		# do hierarchical clustering to make it look pretty
 		D1 = squareform(pdist(df, metric='euclidean'))
 		D2 = squareform(pdist(df.T, metric='euclidean'))
 		Y = linkage(D1, method='complete')
 		Z1 = dendrogram(Y, orientation='right')
 		Y = linkage(D2, method='complete')
-    		Z2 = dendrogram(Y)
+		Z2 = dendrogram(Y)
 		idx1 = Z1['leaves']
 		idx2 = Z2['leaves']
 		D = df.iloc[idx1, :]
-    		D = D.iloc[:, idx2]
+		D = D.iloc[:, idx2]
 		to_r = Heatmap(
 		        z = [ D.loc[i].tolist() for i in D.index.tolist() ],
 		        y = D.index.tolist(),
@@ -644,12 +671,19 @@ def plotExpression( data, plot_type = "line", ipynb = False, zlim = None ):
 		fig = Figure(data=to_plot, layout=layout)
 
 	elif plot_type == "heatmap":
-		to_plot = to_heatmap(data)
+		to_plot = to_heatmap(data, zlim)
 		fig = Data( [ to_plot ] )
+	elif plot_type == "boxplot":
+		to_plot = to_box(data, boxpoints)
+		fig = to_plot
+	else:
+		print "ERROR: Cannot recognize plot_type = %s" % plot_type
+		return None
 
 
 	if not ipynb:
-		py.plot( fig )
+		unique_url = py.plot( fig )
+		return unique_url
 
 	return fig
 
