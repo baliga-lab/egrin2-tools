a5d65de93f8f3420a9fb1d7d9ec16ec2e256610b
Wei-ju Wu <weiju.wu@gmail.com>
2015-06-05 12:29:01 -0700

    KBase integration
    The Cmonkey splitter can now receive its log directory from the environment. If
    LOG_DIRECTORY is set, the specified directory is used to write the log file

--- build/ensemble.py
+++ build/ensemble.py
@@ -239,7 +239,11 @@ of runs at the requested exclusion rate. Maximum exclusion rate for %d runs is %
             run_df.loc[i, "ncols"] = len(self.run_composition[i]["cols"])
             run_df.loc[i, "excluded"] = self.run_composition[i]["excluded"]
             run_df.loc[i, "blocks"] = (":::").join(self.run_composition[i]["blocks"])
-            run_df.loc[i, "cols"] = (":::").join(self.run_composition[i]["cols"])
+            try:
+                run_df.loc[i, "cols"] = (":::").join(self.run_composition[i]["cols"])
+            except:
+                logging.exception("buggy run composition")
+                raise
 
         run_df.to_csv(path_or_buf=open("%sruns.csv" % path_prefix, mode="w"),
                       columns=["ncols", "excluded", "blocks", "cols"],
