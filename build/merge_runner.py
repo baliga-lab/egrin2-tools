b1dd2ba7d2f5d96dd78337070767a1d349a9b70c
Wei-ju Wu <weiju.wu@gmail.com>
2015-06-24 14:55:17 -0700

    merge_runner argument list adjusted

--- build/merge_runner.py
+++ build/merge_runner.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python
 import argparse
+import logging
+import os
+
+DESCRIPTION = "merge_runner.py - merge multiple cmonkey results"
+
+LOG_FORMAT = '%(asctime)s %(levelname)-8s %(message)s'
+LOG_LEVEL = logging.DEBUG
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=DESCRIPTION)
     parser.add_argument('--organism', required=True)
-    parser.add_argument('--ncbi_code', required=True)
     parser.add_argument('--ratios', required=True)
+    parser.add_argument('result_dbs', nargs='*')
     args = parser.parse_args()
-    
+
+    if 'LOG_DIRECTORY' in os.environ:
+        logfile = os.path.join(os.environ['LOG_DIRECTORY'], 'merge_runner.log')
+    else:
+        logfile = None
+    logging.basicConfig(format=LOG_FORMAT, datefmt='%Y-%m-%d %H:%M:%S',
+                        level=LOG_LEVEL, filename=logfile)
+
+    logging.info("Running merge...")
+    logging.info(args.result_dbs)
+
