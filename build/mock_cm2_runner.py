e95151bdc8139a9d8788a5815e96b20ae3c3353a
Wei-ju Wu <weiju.wu@gmail.com>
2015-06-24 13:55:15 -0700

    added mock standins for EGRIN2 tools

---
+++ build/mock_cm2_runner.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import argparse
+
+"""
+A mock cm2_runner. It accepts the same input parameters and generates
+the same output file, but it does nothing.
+"""
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description=DESCRIPTION)
+  parser.add_argument('--organism', required=True)
+  parser.add_argument('--inputfile', required=True)
+  parser.add_argument('--run_num', type=int, required=True)
+  parser.add_argument('--outdb', required=True)
+  args = parser.parse_args()
+    
+  with open(args.outdb, 'w') as outfile:
+    outfile.write('this is empty')
+
