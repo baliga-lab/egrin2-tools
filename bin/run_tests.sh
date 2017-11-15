16404946bbaeaaa81640c9f9c4db8656874aac60
Wei-ju Wu <weiju.wu@gmail.com>
2017-11-15 15:29:51 -0800

    first version of gh-pages

--- bin/run_tests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-PYTHONPATH=`pwd` test/query_test.py $@ && PYTHONPATH=`pwd` test/assembler_test.py $@ && \
-    PYTHONPATH=`pwd` test/util_test.py $@ && PYTHONPATH=`pwd` test/resample_test.py $@
